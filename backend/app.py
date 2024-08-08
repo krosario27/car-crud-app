66367a13a3f3f472d3b8c73e12b6fda07071df84
Kyle <kylewhite369@gmail.com>
2024-08-08 17:11:38 +1200

    backend connected to database

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,19 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+# Connecto to MongoDB Database
+app.config['MONGO_URI'] = "mongodb://127.0.0.1:27017/car_db"
+mongo = PyMongo(app)
+
+#Define parse_json function
+def parse_json(data):
+    return json.loads((json_util.dumps(data)))
+
+
+@app.route("/api", methods=["GET"])
+def api():
+    return jsonify({"response": "api worked..."}), 200
+
 
 
 
