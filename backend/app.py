ae606cc8a1ce2f6b98331bd0caeeabb4c56b2572
Kyle <kylewhite369@gmail.com>
2024-08-08 16:52:52 +1200

    Created env and installed dependencies

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
-print("Hello World")
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from bson.objectid import ObjectId
+from bson import json_util 
+import json
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+
+
